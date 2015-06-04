bd8a1a39b0089064aee857e6307a53452ae0ee8a
budickda <daniel@budick.eu>
2015-06-04 12:36:14 +0200

    quickly added a timeout event to scene_one to show that it is possible...

--- client/scene_one.js
+++ client/scene_one.js
@@ -5,8 +5,17 @@
 var sceneOne = new Scene('rainy');
 sceneOne.addText('Du wachst in einem dunklen Raum auf, was Wasser rieselt von der Decke.')
 //we can save the object behind a keyword like this, to manipulate it
-var holztisch = sceneOne.createKeyword('Der Raum ist dunkel, in der Mitte steht ein nasser [Holztisch]. ');
-//Holztisch has now the effect 'wet', exactly like we wrote it in the text.
-holztisch.hasEffect('wet');
-sceneOne.createKeyword('Auf diesem liegt ein silbrig glänzendes [Schwert].')
+var holztisch = sceneOne.createKeyword('Der Raum ist dunkel, in der Mitte steht ein alter, nasser und halbverotteter [Holztisch]. ');
+//Holztisch has now the effect 'rotten' and 'old', exactly like we wrote it in the text.
+holztisch.hasEffect('rotten');
+holztisch.hasEffect('old');
 sceneOne.updateText();
+
+//this will be printed after 5 seconds:
+Meteor.setTimeout(function(){
+    sceneOne.createKeyword('Auf diesem liegt ein silbrig glänzendes [Schwert].');
+    sceneOne.updateText();
+}, 5000)
+
+
+
